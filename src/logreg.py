602c560a62b12f35b41cb3fb379ac21c3f2c6044
Rohan Karamel <rak218@scarletmail.rutgers.edu>
2025-03-20 16:25:46 -0400

    logreg test changes

--- src/logreg.py
+++ src/logreg.py
@@ -11,18 +11,14 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=16)
 
-# import the class
 from sklearn.linear_model import LogisticRegression
 
-# instantiate the model (using the default parameters)
 logreg = LogisticRegression(random_state=16)
 
-# fit the model with data
 logreg.fit(X_train, y_train)
 
 y_pred = logreg.predict(X_test)
 
-# import the metrics class
 from sklearn import metrics
 
 cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
